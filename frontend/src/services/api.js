a30b278fcf63527af69f94bd531f60091c1f30cc
Luvdeep <luvdeepbeniwal07@gmail.com>
2025-06-19 23:23:10 +0530

    first commit

---
+++ frontend/src/services/api.js
@@ -0,0 +1,13 @@
+const API_KEY = "046b775e5e2d2f17b1fa4d31bf529b64";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json()
+  return data.results
+};
+export const searchMovies = async (query) => {
+  const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+  const data = await response.json()
+  return data.results
+};
